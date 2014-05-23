7af72d3cb005c2433cf285519d21d02750ce5eec
Siegfried Rasthofer <siegfried.rasthofer@cased.de>
2014-05-23 17:18:44 +0200

    added source and apk of poc exploit

---
+++ DenialOfAppPoC_Source/src/de/ecspride/Settings.java
@@ -0,0 +1,11 @@
+package de.ecspride;
+
+public class Settings {
+	public static final String manifestFile = "AndroidManifest.xml";
+	public static final String manifestFilePathInAssets = "manifest/AndroidManifest.xml";
+	
+	public static final String brokenApkFolderName = "brokenAPK";
+	public static final String SIGNFOLDER = "/sign/";
+	public static final String INSTALLAPK = "install.apk";
+	public static String workingDir = null;
+}
